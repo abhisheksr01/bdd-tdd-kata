7cc24eddbd13c7a6a1a44548258ea0674ead680d
abhisheksr01 <abhisheksingh2691@gmail.com>
2022-04-05 09:08:22 +0100

    feat: update student details

--- src/main/java/com/kata/bddtdd/StudentDatabase.java
+++ src/main/java/com/kata/bddtdd/StudentDatabase.java
@@ -12,17 +12,19 @@ public class StudentDatabase {
     public List<Student> getStudentDetailsFromDB(String namePrefix) {
         List<Student> studentList = new ArrayList<>();
         Student student = new Student("abhishek", "rajput");
-        Student student1 = new Student("nagina", "rafique");
-        Student student2 = new Student("chaam", "zeina");
-        Student student3 = new Student("sabi", "dhiman");
-        Student student4 = new Student("kun", "zhang");
-        Student student5 = new Student("rashid", "fagan-walcott");
+        Student student1 = new Student("novita", "s");
+        Student student2 = new Student("shin", "chan");
+        Student student3 = new Student("ash", "ketchum");
+        Student student4 = new Student("john", "cena");
+        Student student5 = new Student("roger", "federer");
+        Student student6 = new Student("david", "beckham");
         studentList.add(student);
         studentList.add(student1);
         studentList.add(student2);
         studentList.add(student3);
         studentList.add(student4);
         studentList.add(student5);
+        studentList.add(student6);
 
         return studentList.stream().filter(
                 studentDetails -> studentDetails.getFirstName().startsWith(namePrefix)
